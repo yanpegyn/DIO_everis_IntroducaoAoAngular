15d5f7b41365f2ee7770607d4ac342b66789758a
Yan Gustavo Pegyn Silva <46832714+yanpegyn@users.noreply.github.com>
2021-01-10 22:53:17 -0300

    Finalizado

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -16,8 +16,27 @@ export class CourseListComponent implements OnInit {
     constructor(private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this._courses = this.courseService.retriveAll();
-        this.filteredCourses = this._courses;
+        this.retriveAll();
+    }
+
+    retriveAll(): void {
+        this.courseService.retriveAll().subscribe({
+            next: courses => {
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+            },
+            error: err => console.log('Error', err)
+        });
+    }
+
+    deleteById(couserId: number): void {
+        this.courseService.deleteById(couserId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retriveAll();
+            },
+            error: err => console.log('Error', err)
+        })
     }
 
     set filter(value: string) {
